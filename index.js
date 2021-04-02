dadd104478f2dcedef7fd6c02b2e97764b87fb6d
ratnakarmaker <73543192+ratnakarmaker@users.noreply.github.com>
2021-04-02 17:21:03 +0600

    delete product

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello Everyone!')
@@ -27,66 +27,75 @@ client.connect(err => {
   const itemCollection = client.db(`${process.env.DB_NAME}`).collection("item");
   const usersCollection = client.db(`${process.env.DB_NAME}`).collection("users");
 
-  app.get('/events', (req, res)=> {
+  app.get('/events', (req, res) => {
     itemCollection.find({})
-    .toArray((err, document)=> {
-      res.send(document)
-    })
+      .toArray((err, document) => {
+        res.send(document)
+      })
   })
 
-  app.get('/selectedProduct/:id', (req, res)=> {
-    itemCollection.find({_id: objectId(req.params.id)})
-    .toArray((err, document)=> {
-      res.send(document)
-    })
+  app.get('/selectedProduct/:id', (req, res) => {
+    itemCollection.find({ _id: objectId(req.params.id) })
+      .toArray((err, document) => {
+        res.send(document)
+      })
   })
+
+  // delete product
+  app.delete("/delete/:id", (req, res) => {
+    productsCollection.findOneAndDelete({ _id: objectId(req.params.id) })
+      .then(result => {
+        res.send(result.deletedCount > 0)
+      })
+  })
+
 
   // update product
-  app.patch('/update/:id', (req,res) => {
-    productsCollection.updateOne({_id: objectId(req.params.id)},
-    {
-      $set: {
-        name: req.body.name,
-        price: req.body.price,
-        image: req.body.image
-      }
-    })
-    .then(result => {
-      res.send(result.modifiedCount > 0)
-    })
-})
+  app.patch('/update/:id', (req, res) => {
+    productsCollection.updateOne({ _id: objectId(req.params.id) },
+      {
+        $set: {
+          name: req.body.name,
+          price: req.body.price,
+          image: req.body.image
+        }
+      })
+      .then(result => {
+        res.send(result.modifiedCount > 0)
+      })
+  })
 
 
-  app.post('/addEvent', (req, res)=> {
+  app.post('/addEvent', (req, res) => {
     const eventDetails = req.body;
     itemCollection.insertOne(eventDetails)
-    .then(result => {
-      res.status(200).send('Inserted');
-      console.log('inserted :' , result.insertedCount);
-    })
+      .then(result => {
+        res.status(200).send('Inserted');
+        console.log('inserted :', result.insertedCount);
+      })
   })
- 
+
 
   // create new user
-  app.post('/signup', (req, res)=> {
+  app.post('/signup', (req, res) => {
     const userData = req.body;
     usersCollection.insertOne(userData)
-    .then(result => res.status(200).send('User Created'))
+      .then(result => res.status(200).send('User Created'))
   })
 
   // login user
-  app.get('/signin', (req, res)=> {
+  app.get('/signin', (req, res) => {
     const userData = req.query;
     usersCollection.find(userData)
-    .toArray((err, user)=> {
-      if(user.length === 0){
-        res.status(400)
-        res.send({"error": "Wrong Eamil Or Password"})
-      }
-      res.status(200);
-      res.send(user[0]);
-      
-    })
+      .toArray((err, user) => {
+        if (user.length === 0) {
+          res.status(400)
+          res.send({ "error": "Wrong Eamil Or Password" })
+        }
+        res.status(200);
+        res.send(user[0]);
+
+      })
   })
 
 });
